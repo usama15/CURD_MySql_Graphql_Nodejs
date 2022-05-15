95fa22b07a07e2ee3b534207750a5d56a7fc43da
usama <princeusamaansari15@gmail.com>
2022-05-15 17:59:06 +0500

    error handling

--- server/src/Schema/Mutations/User.ts
+++ server/src/Schema/Mutations/User.ts
@@ -1,42 +1,47 @@
-import { GraphQLID, GraphQLString } from "graphql";
-import { UsersType } from "../TypeDefs/User";
-import { MessageType } from "../TypeDefs/Messages";
-import { Users } from "../../Entities/User";
+import {GraphQLID, GraphQLString} from "graphql";
+import {UsersType} from "../TypeDefs/User";
+import {MessageType} from "../TypeDefs/Messages";
+import {Users} from "../../Entities/User";
 
 
 export const CREATE_USER = {
     type: UsersType,
     args: {
-        name: { type: GraphQLString },
-        username: { type: GraphQLString },
-        password: { type: GraphQLString }
+        name: {type: GraphQLString},
+        username: {type: GraphQLString},
+        password: {type: GraphQLString}
     },
     async resolve(parent: any, args: any) {
-        const { name, username, password } = args
-        await Users.insert({ name, username, password })
+        const {name, username, password} = args
+        await Users.insert({name, username, password})
 
         return args
     }
 }
 
 
-
 export const UPDATE_PASSWORD = {
-    type: UsersType,
+    type: MessageType,
     args: {
-        username: { type: GraphQLString },
-        oldPassword: { type: GraphQLString },
-        newPassword: { type: GraphQLString }
+        id: {type: GraphQLID},
+        oldPassword: {type: GraphQLString},
+        newPassword: {type: GraphQLString}
     },
     async resolve(parent: any, args: any) {
-        const { id, oldPassword, newPassword } = args
-        const user = await Users.findOne({ id: id })
+        const {id, oldPassword, newPassword} = args
+        const user = await Users.findOne({id: id})
+        if(!user){
+            throw new Error("User not found!")
+        }
+
         const userPassword = user?.password
 
         if (oldPassword == userPassword) {
-            await Users.update({ id: id }, { password: newPassword })
+            await Users.update({id: id}, {password: newPassword})
+            return {success: true, message: "Password update"}
         } else {
-            throw new Error("Password does not match!")
+            return {success: true, message: "Password does not match!"}
+            // throw new Error("Password does not match!")
         }
 
     }
@@ -44,12 +49,17 @@ export const UPDATE_PASSWORD = {
 export const DELETE_USER = {
     type: MessageType,
     args: {
-        id: { type: GraphQLID }
+        id: {type: GraphQLID}
     },
     async resolve(parent: any, args: any) {
-        const { id } = args
-        await Users.delete(id)
-        return{success: true, message: "Delete user"}
+        const {id} = args
+        const user = await Users.findOne({id: id})
+        if (user?.id == id) {
+            await Users.delete(id)
+            return {success: true, message: "Delete user"}
+        } else {
+            return {success: false, message: "User not found"}
+        }
     }
 }
 
