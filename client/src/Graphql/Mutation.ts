83196c7cca0cfc0be103b1adf50c0a3ea1f0d72b
usama <princeusamaansari15@gmail.com>
2022-06-09 18:25:33 +0500

    client update

---
+++ client/src/Graphql/Mutation.ts
@@ -0,0 +1,16 @@
+import {gql} from "@apollo/client"
+
+export const CREATE_USER = gql`
+    mutation createUser(
+    $name:String! ,
+    $username: String!, 
+    $password: String!){
+        createUser(name:$name 
+                   username: $username 
+                   password: $password){
+                        id
+                        name
+                        username
+                   }
+    }
+    `
